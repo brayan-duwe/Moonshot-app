729cb6b34826e296591b2e3439bf8cc08995b649
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-04-12 19:20:36 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Brayan Duwe on 12/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

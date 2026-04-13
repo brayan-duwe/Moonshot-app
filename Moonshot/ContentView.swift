5293af344a0c9fe43191acf28a208e996ab27943
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-04-13 19:03:12 -0300

    pushing new views with NavigationLink

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,29 +7,15 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText(text: "Item \($0)")
-                        .font(.title)
+        NavigationStack {
+            List(0..<100) {row in
+                NavigationLink("Row \(row)") {
+                    Text("Detail \(row)")
                 }
             }
-            .frame(maxWidth: .infinity)
+            .navigationTitle("SwiftUI")
         }
     }
 }

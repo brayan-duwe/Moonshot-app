4b3c928df324ee7fd165e4792f094ac9d82f552a
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-04-15 21:06:56 -0300

    Moon. first app code

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Brayan Duwe on 15/04/26.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,12 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Brayan Duwe on 15/04/26.
+//
+
+import Foundation
+
+extension Bundle {
+    
+}
